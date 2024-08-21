999fb8e6866580c1a3df6ca3a319e32eb87ccef0
krukd <krukd87@gmail.com>
2024-08-21 13:01:31 +0800

    Data fetching (useEffect)

--- src/context.jsx
+++ src/context.jsx
@@ -1,14 +1,26 @@
-import React, {useContext} from 'react'
+import React, { useContext, useEffect } from "react";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
-    return <AppContext.Provider value="hello">{children}</AppContext.Provider>
-}
+const AppProvider = ({ children }) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://randomuser.me/api");
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  useEffect(() => {
+    fetchData()
+  }, []);
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
 
 //кастомный хук контекст
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export {AppContext, AppProvider}
+export { AppContext, AppProvider };
