723ea4a47404fb3f0ae1740435808769a2c308fd
krukd <krukd87@gmail.com>
2024-08-21 13:53:39 +0800

    Axios installed

--- src/context.jsx
+++ src/context.jsx
@@ -1,19 +1,22 @@
 import React, { useContext, useEffect } from "react";
+import axios from 'axios';
 
 const AppContext = React.createContext();
 
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
 const AppProvider = ({ children }) => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://randomuser.me/api");
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const fetchMeals = async (url) => {
+    try {
+      const {data} = await axios(url);
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
   useEffect(() => {
-    fetchData()
+    fetchMeals(allMealsUrl);
   }, []);
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
